129a203f03b4a7f5e99db23103b161524f4011fe
minhReal <duongminhminh46@gmail.com>
2025-02-02 19:15:58 +0700

    Update Test_script.lua

--- Test_script.lua
+++ Test_script.lua
@@ -1,9 +1,20 @@
 local gameId = game.PlaceId
+local scripts = {
+    [2882332175] = "https://raw.githubusercontent.com/minhReal/mainS/refs/heads/main/Script/BDFS_NewUi.lua",
+    [537413528] = "https://raw.githubusercontent.com/minhReal/mainS/refs/heads/main/Script/BABFT.lua"
+}
 
-if gameId == 2882332175 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/minhReal/mainS/refs/heads/main/Script/BDFS_NewUi.lua"))()
-elseif gameId == 537413528 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/minhReal/mainS/refs/heads/main/Script/BABFT.lua"))()
+local URL = scripts[gameId]
+
+if URL then
+    local success, err = pcall(function()
+        loadstring(game:HttpGet(url))()
+    end)
+    sendnotification({
+        Title = success and "Notification" or "Error",
+        Text = success and "Script has been loaded successfully!" or "Failed to load script: " .. err,
+        Duration = 5
+    })
 else
     sendnotification({
         Title = "Notification",
