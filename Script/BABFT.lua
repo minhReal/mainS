1944d435d803b7f0333f16852065c205448e949c
minhReal <duongminhminh46@gmail.com>
2025-02-04 01:08:34 +0700

    Update BABFT.lua

--- Script/BABFT.lua
+++ Script/BABFT.lua
@@ -1,1 +1,2 @@
+-- I am not the one who made this script. I just took this script for testing --
 loadstring(game:HttpGet('https://raw.githubusercontent.com/TheRealAsu/BABFT/refs/heads/main/Source.lua'))()
