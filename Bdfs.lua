27af7480d87c9509343be59766757e98f7a0563d
minhReal <duongminhminh46@gmail.com>
2025-02-04 00:31:55 +0700

    Update Bdfs.lua

--- Bdfs.lua
+++ Bdfs.lua
@@ -1,4 +1,5 @@
--- bản #2 --
+-- Bản #2 / Version #2 --
+-- Bản cũ \ old version --
 local function typeText(object, text, delay)
     for i = 1, #text do
         object.Text = text:sub(1, i)
@@ -63,14 +64,12 @@ moneyHitbox.Color = Color3.new(1, 1, 1)
 moneyHitbox.Size = Vector3.new(9.5, 50, 9.5)
 
 local gui = Instance.new("ScreenGui")
-gui.Name = "patrickGui"
+gui.Name = "187369187619e918"
 gui.Parent = game.CoreGui
 
--- Quản lý Animation
 local currentAnimation
 local isAnimating = false
 
--- Hàm phát animation
 local function playAnimation(animationId)
     local character = player.Character or player.CharacterAdded:Wait()
     if character then
@@ -79,16 +78,15 @@ local function playAnimation(animationId)
             local animation = Instance.new("Animation")
             animation.AnimationId = animationId
             if currentAnimation then
-                currentAnimation:Stop() -- Dừng animation hiện tại
+                currentAnimation:Stop()
             end
             currentAnimation = humanoid:LoadAnimation(animation)
             currentAnimation:Play()
-            isAnimating = true -- Đánh dấu đang phát animation
+            isAnimating = true
         end
     end
 end
 
--- Tạo ScrollingFrame
 local function createScrollingFrame(position)
     local frame = Instance.new("ScrollingFrame")
     frame.Size = UDim2.new(0.2, 0, 1, 0)
@@ -98,7 +96,7 @@ local function createScrollingFrame(position)
     frame.BorderSizePixel = 0
     frame.BackgroundTransparency = 0.6
     frame.Visible = false
-    frame.CanvasSize = UDim2.new(0, 0, 50, 0) -- Điều chỉnh kích thước canvas để có thể cuộn nhiều hơn
+    frame.CanvasSize = UDim2.new(0, 0, 50, 0)
     return frame
 end
 
@@ -110,18 +108,17 @@ main.Parent = gui
 main2.Parent = gui
 main3.Parent = gui
 
--- Biến theo dõi vị trí Y cuối cùng cho mỗi khung
 local lastButtonY_main = 0
 local lastButtonY_main2 = 0
 local lastButtonY_main3 = 0
-local lastButtonY_autofarm = 0 -- Vị trí cho nút Autofarm
+local lastButtonY_autofarm = 0
 local buttonOffset = 0.00285
 
--- Tạo nút trong GUI với vị trí tự động
+
 local function createButton(parent, text, callback, frameId)
     local button = Instance.new("TextButton")
     button.Size = UDim2.new(0.9, 0, 0.0025, 0)
-    button.Position = UDim2.new(0.05, 0, (frameId == 1 and lastButtonY_main or frameId == 2 and lastButtonY_main2 or lastButtonY_main3), 0) -- Sử dụng lastButtonY tương ứng với khung
+    button.Position = UDim2.new(0.05, 0, (frameId == 1 and lastButtonY_main or frameId == 2 and lastButtonY_main2 or lastButtonY_main3), 0)
     button.BackgroundColor3 = Color3.new(0, 0, 0)
     button.BorderColor3 = Color3.new(1, 1, 1)
     button.BorderSizePixel = 1
@@ -133,7 +130,6 @@ local function createButton(parent, text, callback, frameId)
     button.Parent = parent
     button.MouseButton1Click:Connect(callback)
 
-    -- Cập nhật lastButtonY cho nút tiếp theo tương ứng với khung
     if frameId == 1 then
         lastButtonY_main = lastButtonY_main + buttonOffset
     elseif frameId == 2 then
@@ -145,7 +141,6 @@ local function createButton(parent, text, callback, frameId)
     return button
 end
 
--- Nút cho các hành động khác nhau trong khung chính
 createButton(main, "Meshes/Medkit", function()
     fireclickdetector(workspace["Meshes/Medkit"].ClickDetector)
 end, 1)
@@ -179,7 +174,6 @@ createButton(main, "Knife", function()
     end
 end, 1)
 
--- Tạo các nút trong main2
 createButton(main2, "Anim(Bat)", function()
     playAnimation("rbxassetid://8830424363")
 end, 2)
@@ -205,10 +199,10 @@ createButton(main2, "Esp the dead", function()
         if corpse:IsA("Model") then
             local highlight = Instance.new("Highlight")
             highlight.Parent = corpse
-            highlight.FillColor = Color3.fromRGB(255, 0, 0) -- Màu đỏ
-            highlight.OutlineColor = Color3.fromRGB(255, 255, 255) -- Viền trắng 
-            highlight.FillTransparency = 0.8 -- Độ trong suốt
-            highlight.OutlineTransparency = 0.2 -- Không trong suốt
+            highlight.FillColor = Color3.fromRGB(255, 0, 0)
+            highlight.OutlineColor = Color3.fromRGB(255, 255, 255)
+            highlight.FillTransparency = 0.8
+            highlight.OutlineTransparency = 0.2
         end
     end
 end, 2)
@@ -238,23 +232,20 @@ createButton(main2, "Rejoin", function()
     TeleportService:Teleport(game.PlaceId, player)
 end, 2)
 
--- Tạo các nút trong main3
 local UserInputService = game:GetService("UserInputService")
 local VirtualInputManager = game:GetService("VirtualInputManager")
 local player = game.Players.LocalPlayer
 local hungerGui = player.PlayerGui:WaitForChild("Hunger")
 local burgerClickDetector = workspace.Buildings.DeadBurger.burgre.ClickDetector
 
--- Hàm xóa Trashcan không có trẻ em và lấy rác
 function cleanUpTrashcans()
     for _, v in pairs(workspace:GetChildren()) do
         if v.Name == "Trashcan" and v:IsA("UnionOperation") then
             if #v:GetChildren() ~= 2 then
-                v:Destroy() -- Xóa Trashcan nếu không có 2 con cái
+                v:Destroy()
             else
-                -- Lấy rác từ Trashcan
                 if v:FindFirstChild("ClickDetector") then
-                    fireclickdetector(v.ClickDetector) -- Click vào Trashcan để lấy rác
+                    fireclickdetector(v.ClickDetector)
                 end
             end
         end
@@ -263,20 +254,18 @@ end
 
 local function eat()
     if not player.Backpack:FindFirstChild("Burger") then
-        fireclickdetector(burgerClickDetector) -- Lấy hamburger
+        fireclickdetector(burgerClickDetector)
     end
     wait(0.1)
 
-    local _burger = player.Backpack:WaitForChild("Burger") -- Chờ hamburger được lấy
-    _burger.Parent = player.Character -- Trang bị hamburger
-    _burger:Activate() -- Ăn hamburger
-    wait(1.5) -- Thời gian ăn
+    local _burger = player.Backpack:WaitForChild("Burger")
+    _burger.Parent = player.Character
+    _burger:Activate()
+    wait(1.5)
 end
 
--- Hàm chạy autofarm
-local autofarmActive = false -- Khởi tạo trạng thái
+local autofarmActive = false
 
--- Autofarm cho máy tính trong Green House
 local function runComputerAutofarm()
     while autofarmActive do
         wait(0.01)
@@ -284,19 +273,15 @@ local function runComputerAutofarm()
     end
 end
 
--- Sửa đổi hàm runAutofarm để bao gồm cuộc gọi đến runComputerAutofarm
 local function runAutofarm()
-    autofarmActive = true -- Đặt thành hoạt động khi được gọi
-
-    -- Di chuyển đến vị trí cụ thể
+    autofarmActive = true
     spawn(function()
         while autofarmActive do
             wait(0.01)
-            player.Character.HumanoidRootPart.CFrame = CFrame.new(-122.55330657958984, -3.750000476837158, 165.34146118164062) -- teleport đến vị trí chỉ định liên tục
+            player.Character.HumanoidRootPart.CFrame = CFrame.new(-122.55330657958984, -3.750000476837158, 165.34146118164062)
         end
     end)
 
-    -- Gọi hàm chạy autofarm cho máy tính
     spawn(runComputerAutofarm)
 
     while autofarmActive do
@@ -305,20 +290,18 @@ local function runAutofarm()
         wait(0.01)
         VirtualInputManager:SendKeyEvent(true, "Two", false, player.Character:FindFirstChild("Humanoid"))
 
-        -- Gọi hàm để xóa và lấy rác từ Trashcan
         cleanUpTrashcans()
 
-        -- Kiểm tra và ăn hamburger nếu mức đói dưới 50
         if hungerGui and hungerGui.Hunger.Value < 50 then
-            eat() -- Gọi hàm eat nếu hunger dưới 50
-            wait(2.5) -- Thời gian chờ sau khi ăn
+            eat()
+            wait(2.5)
         end
 
-        wait(0.01) -- Thời gian chờ giữa các lần kiểm tra tổng quát
+        wait(0.01)
     end
 end
 
--- Tạo nút Autofarm với chức năng chuyển đổi
+
 local function createAutofarmButton()
     local autofarmToggle = Instance.new("TextButton")
     autofarmToggle.Size = UDim2.new(0.9, 0, 0.0025, 0)
@@ -336,13 +319,12 @@ local function createAutofarmButton()
     lastButtonY_autofarm = lastButtonY_autofarm + buttonOffset
 
     autofarmToggle.MouseButton1Click:Connect(function()
-        autofarmActive = not autofarmActive -- Chuyển đổi trạng thái
+        autofarmActive = not autofarmActive
         autofarmToggle.TextColor3 = autofarmActive and Color3.new(0, 1, 0) or Color3.new(1, 0, 0)
 
         if autofarmActive then
-            runAutofarm() -- Gọi hàm khi được kích hoạt
+            runAutofarm()
         else
-            -- Teleport đến một vị trí ngẫu nhiên trong Spawn khi tắt autofarm
             local spawns = workspace.Spawns:GetChildren()
             if #spawns > 0 then
                 local randomSpawn = spawns[math.random(1, #spawns)]
@@ -353,66 +335,62 @@ local function createAutofarmButton()
     end)
 end
 
-createAutofarmButton() -- Tạo nút
+createAutofarmButton()
 
 
 local PlayerGui = player:WaitForChild("PlayerGui")
 
--- Kiểm tra nếu GUI Hunger tồn tại
 local hungerGui = PlayerGui:FindFirstChild("Hunger")
 if not hungerGui then
     return
 end
 
--- Tạo GUI để hiển thị Info
 local displayGui = Instance.new("ScreenGui")
 displayGui.Name = "DisplayInfoGui"
 displayGui.Parent = PlayerGui
 
 local infoFrame = Instance.new("Frame")
-infoFrame.Size = UDim2.new(0.3, 0, 0.05, 0) -- Điều chỉnh kích thước của frame
-infoFrame.Position = UDim2.new(0.28, 0, 0.958, 0) -- Vị trí của frame
+infoFrame.Size = UDim2.new(0.3, 0, 0.05, 0)
+infoFrame.Position = UDim2.new(0.28, 0, 0.958, 0)
 infoFrame.BackgroundColor3 = Color3.new(0, 0, 0)
 infoFrame.BackgroundTransparency = 1
 infoFrame.Parent = displayGui
 infoFrame.Visible = false
 
 local hungerLabel = Instance.new("TextLabel")
-hungerLabel.Size = UDim2.new(1, 0, 0) -- Kích thước cho Hunger 
+hungerLabel.Size = UDim2.new(1, 0, 0)
 hungerLabel.Position = UDim2.new(-0.2, -0.55, 0)
 hungerLabel.BackgroundTransparency = 1
 hungerLabel.TextColor3 = Color3.new(1, 1, 1)
 hungerLabel.Font = Enum.Font.Code
 hungerLabel.TextSize = 18
-hungerLabel.TextXAlignment = Enum.TextXAlignment.Left -- Căn trái
-hungerLabel.TextStrokeTransparency = 0 -- Đặt độ trong suốt outline
-hungerLabel.TextStrokeColor3 = Color3.fromRGB(0, 0, 0) -- Màu của outline
+hungerLabel.TextXAlignment = Enum.TextXAlignment.Left
+hungerLabel.TextStrokeTransparency = 0
+hungerLabel.TextStrokeColor3 = Color3.fromRGB(0, 0, 0)
 hungerLabel.Parent = infoFrame
 
 local joyLabel = Instance.new("TextLabel")
-joyLabel.Size = UDim2.new(1, 0, 0) -- Kích thước cho Joy
+joyLabel.Size = UDim2.new(1, 0, 0)
 joyLabel.Position = UDim2.new(1.2, -0.55, 0)
 joyLabel.BackgroundTransparency = 1
 joyLabel.TextColor3 = Color3.new(1, 1, 1)
 joyLabel.Font = Enum.Font.Code
 joyLabel.TextSize = 18
-joyLabel.TextXAlignment = Enum.TextXAlignment.Left -- Căn trái
-joyLabel.TextStrokeTransparency = 0 -- Đặt độ trong suốt outline
-joyLabel.TextStrokeColor3 = Color3.fromRGB(0, 0, 0) -- Màu của outline
+joyLabel.TextXAlignment = Enum.TextXAlignment.Left
+joyLabel.TextStrokeTransparency = 0
+joyLabel.TextStrokeColor3 = Color3.fromRGB(0, 0, 0)
 joyLabel.Parent = infoFrame
 
--- Cập nhật giá trị Info từ PlayerGui
+
 local function updateInfo()
     local hungerValue = hungerGui.Hunger.Value
     
-    -- Lấy giá trị Joy từ LocalPlayer và định dạng
     local joyValue = player.Character:WaitForChild("Values"):WaitForChild("Joy").Value 
-    hungerLabel.Text = "Hunger: " .. hungerValue -- Hiển thị Hunger
+    hungerLabel.Text = "Hunger: " .. hungerValue 
 
-    -- Định dạng Joy với 3 chữ số thập phân
     local joyText = string.format("Joy: %.3f - ", joyValue)
 
-    -- Thêm biểu tượng cảm xúc dựa trên giá trị Joy
+
     if joyValue < 50 then
         joyText = joyText .. "Not good"
     else
@@ -423,17 +401,15 @@ local function updateInfo()
 end
 
 
--- Kết nối sự kiện cho việc thay đổi Hunger và Joy
 hungerGui.Hunger:GetPropertyChangedSignal("Value"):Connect(updateInfo)
 player.Character.Values.Joy:GetPropertyChangedSignal("Value"):Connect(updateInfo)
 
--- Khởi động lần đầu tiên
+
 updateInfo()
 
--- Thêm màu cho nút infoLabel
 local infoLabelBtn = Instance.new("TextButton")
 infoLabelBtn.Size = UDim2.new(0.9, 0, 0.0025, 0)
-infoLabelBtn.Position = UDim2.new(0.05, 0, lastButtonY_autofarm, 0) -- Vị trí của frame để giống phần Autofarm
+infoLabelBtn.Position = UDim2.new(0.05, 0, lastButtonY_autofarm, 0)
 infoLabelBtn.BackgroundColor3 = Color3.new(0, 0, 0)
 infoLabelBtn.BorderColor3 = Color3.new(1, 1, 1)
 infoLabelBtn.TextSize = 15
