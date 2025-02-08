087aafaed625535cb68e3c43b2d853be7e38f6b6
minhReal <duongminhminh46@gmail.com>
2025-02-09 01:14:06 +0700

    Update Test_script.lua

--- Test_script.lua
+++ Test_script.lua
@@ -1,4 +1,3 @@
--- I made this script first so that maybe I can make another game script using the script --
 local marketplaceService = game:GetService("MarketplaceService")
 local gameInfo = marketplaceService:GetProductInfo(gameId)
 local gameId = game.PlaceId
