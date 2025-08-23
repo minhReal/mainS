14f610503bebe21676e5281506a6ef3a124bd692
minhReal <duongminhminh46@gmail.com>
2025-08-24 06:04:25 +0700

    Update no_cars.lua

--- BDFS_script/no_cars.lua
+++ BDFS_script/no_cars.lua
@@ -1,24 +1,9 @@
 local objectsToVoid = {"goodcar", "goodcar2", "bestmovingcar", "bestmovingcar2"}
-
 local voidPosition = Vector3.new(0, -500, 0)
 
-local voidRunning = true
-
-local function sendToVoid()
-    for _, objectName in ipairs(objectsToVoid) do
-        local object = workspace:FindFirstChild(objectName)
-        if object and object:IsA("BasePart") then
-      
-            object.CFrame = CFrame.new(voidPosition)
-        end
+for _, name in ipairs(objectsToVoid) do
+    local obj = workspace:FindFirstChild(name)
+    if obj and obj:IsA("BasePart") then
+        obj.CFrame = CFrame.new(voidPosition)
     end
 end
-
-spawn(function()
-    while true do
-        if voidRunning then
-            sendToVoid()
-        end
-        wait(0.1)
-    end
-end)
